d354d94339efbc9c137be018fb2713aebcb9e289
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2024-03-17 14:25:33 +0700

    Progress web add

--- HT&CS&JS/Porto/Brain.js
+++ HT&CS&JS/Porto/Brain.js
@@ -1,3 +1,16 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
@@ -15,3 +28,5 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+// Side bars code
